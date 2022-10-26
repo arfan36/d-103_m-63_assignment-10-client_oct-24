0996e895f06de133ac9aaa9bf5fc0b1ec8ffd9e3
arfan36 <arfanur36@gmail.com>
2022-10-26 13:49:15 +0600

    logo, header info updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.png';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut().then(() => {
+        }).catch((err) => {
+            console.error('err', err);
+        });
+    };
+
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand>
+                    <Link to={'/'}>
+                        <img
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt=""
+                        />
+                        <> Learn Web Development</>
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <div className='d-flex align-items-center me-2'>
+                            <Link to={'/courses'}>Courses</Link>
+                        </div>
+                        <div className='d-flex align-items-center me-2'>
+                            <Link to={'/faq'}>FAQ</Link>
+                        </div>
+                        <div className='d-flex align-items-center me-2'>
+                            <Link to={'/blog'}>Blog</Link>
+                        </div>
+                    </Nav>
+                    <Nav>
+                        <div className='d-flex align-items-center'>
+                            {
+                                user?.uid ?
+                                    <>
+                                        <span>{user.displayName}</span>
+                                        <Button variant='light' onClick={handleLogOut}>Log Out</Button>
+                                    </>
+                                    :
+                                    <>
+                                        <Link to={'/login'} className="me-2">Login</Link>
+                                        <Link to={'/register'} className="me-2">Register</Link>
+                                    </>
+                            }
+                        </div>
+                        <div className='d-flex align-items-center'>
+                            <Link to={'/profile'}>
+                                {
+                                    user?.photoURL ?
+                                        <Image
+                                            style={{ height: '30px' }}
+                                            roundedCircle
+                                            src={user?.photoURL}
+                                        ></Image>
+                                        :
+                                        <FaUser></FaUser>
+                                }
+                            </Link>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
