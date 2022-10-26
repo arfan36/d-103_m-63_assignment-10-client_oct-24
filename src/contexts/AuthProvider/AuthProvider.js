09f7af0d4ba7f8cdb864e20c5f6dcf227967780f
arfan36 <arfanur36@gmail.com>
2022-10-26 10:48:34 +0600

    fix problem

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createContext } from 'react';
+import { getAuth, signInWithPopup } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    // const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const ProviderLogin = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    };
+
+    const authInfo = {
+        loading,
+        setLoading,
+        ProviderLogin
+    };
     return (
-        <div>
-            <h2>AuthProvider</h2>
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
